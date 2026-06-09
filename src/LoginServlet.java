8cde8014c0b2e56ab13b1ab856281d170964da2c
hassanbughio <120723509+hassanbughio@users.noreply.github.com>
2026-06-09 04:12:14 -0500

    Update LoginServlet.java
    Fixed LoginServlet with javax imports

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -7,10 +7,23 @@ import java.io.*;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        response.setContentType("text/plain");
-        response.getWriter().println("LOGIN SERVLET WORKING");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        
+        if ("admin".equals(username) && "admin123".equals(password)) {
+            out.println("{\"success\": true, \"message\": \"Login successful\", \"role\": \"ADMIN\"}");
+        } else {
+            out.println("{\"success\": false, \"message\": \"Invalid credentials\"}");
+        }
+        
+        out.flush();
+        out.close();
     }
 }
